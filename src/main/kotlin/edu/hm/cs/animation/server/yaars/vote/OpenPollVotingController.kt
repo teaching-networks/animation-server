7b6e087243338130ebb357a0d166719a22695f51
Bastian <bastian@cmbt.de>
2020-11-09 16:13:13 +0100

    Enhanced filtering and voting for a potential existent answer in OpenPollVotingController

--- src/main/kotlin/edu/hm/cs/animation/server/yaars/vote/OpenPollVotingController.kt
+++ src/main/kotlin/edu/hm/cs/animation/server/yaars/vote/OpenPollVotingController.kt
@@ -5,6 +5,7 @@ import edu.hm.cs.animation.server.util.stomp.subscriptions.STOMPOpenPollSubscrip
 import edu.hm.cs.animation.server.yaars.poll.answer.dao.OpenAnswerDAO
 import edu.hm.cs.animation.server.yaars.poll.answer.model.OpenAnswer
 import edu.hm.cs.animation.server.yaars.poll.dao.OpenPollDAO
+import edu.hm.cs.animation.server.yaars.poll.model.OpenQuestionPoll
 import io.javalin.http.Context
 import kotlin.math.round
 
@@ -32,29 +33,31 @@ object OpenPollVotingController {
             return
         }
 
-        val text = ctx.body<Map<String, String>>()["text"] ?: error("No body with property text!!")
+        val text = ctx.body<Map<String, String>>()["text"] ?: run {
+            ctx.status(400)
+            error("No body with property text!!")
+        }
 
         // checks if this specific poll already has an answer with text = text or if the levenshtein distance
         // is less than 1/5 of the length of the string -> in that case the two strings are viewed as equal and
         // the vote counts towards the string that's already in the database
-        val alreadyVoted = poll.replies.filter { openAnswer ->
+        val filter = { openAnswer: OpenAnswer ->
             val threshold = round(1.0 / 5.0 * openAnswer.text.toLowerCase().length.toDouble())
             val levenshteinDistance = LevenshteinDistanceCalculator
                     .calculateSimilarity(text.toLowerCase(), openAnswer.text.toLowerCase())
 
             openAnswer.text.toLowerCase() == text.toLowerCase() || levenshteinDistance <= threshold
-        }.size == 1
+        }
 
-        // if the set contains such an answer we vote for it, otherwise a new answer is created and the poll is updated
-        if (alreadyVoted) {
-            val answer = poll.replies.filter { openAnswer ->
-                openAnswer.text.toLowerCase() == text.toLowerCase()
-            }[0].answerId
-            openAnswerDAO.vote(answer)
-        } else {
-            val newAnswer = openAnswerDAO.create(OpenAnswer(text = text, timesMentioned = 1, answerId = 0))
-            poll.replies.add(newAnswer)
-            openPollDAO.update(poll)
+        // votes for the existent answer or creates a new answer
+        when (val possibleExistentAnswer = poll.replies.find(filter)) {
+            null -> {
+                val newAnswer = openAnswerDAO
+                        .create(OpenAnswer(text = text, timesMentioned = 1, answerId = 0))
+                poll.replies.add(newAnswer)
+                openPollDAO.update(poll)
+            }
+            else -> openAnswerDAO.vote(possibleExistentAnswer.answerId)
         }
         STOMPOpenPollSubscriptionManager.notifyAboutChange(openPollDAO.find(id))
     }
