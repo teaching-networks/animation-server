f41a141c1eb17aacd993bdbe19dfd8cb1ff9e350
Bastian Kersting <basti@Bastians-MBP.fritz.box>
2020-11-16 19:11:49 +0100

    Added multilined Answer Flag to the data model

--- src/main/kotlin/edu/hm/cs/animation/server/yaars/poll/model/OpenQuestionPoll.kt
+++ src/main/kotlin/edu/hm/cs/animation/server/yaars/poll/model/OpenQuestionPoll.kt
@@ -22,7 +22,10 @@ class OpenQuestionPoll(
         /**
          * List of Available Answers. Every poll is able to have between 2 and 5 questions (OneToMany).
          */
-        @OneToMany(cascade = arrayOf(CascadeType.ALL), mappedBy = "relatedPoll", fetch = FetchType.EAGER)
+        @OneToMany(cascade = [CascadeType.ALL], mappedBy = "relatedPoll", fetch = FetchType.EAGER)
         @JsonManagedReference
-        var replies: MutableSet<OpenAnswer>
+        var replies: MutableSet<OpenAnswer>,
+
+        @Column(nullable=false)
+        var isMultilineAnswer: Boolean = false
 ) : YaarsPoll(id, question, lecture, active)
